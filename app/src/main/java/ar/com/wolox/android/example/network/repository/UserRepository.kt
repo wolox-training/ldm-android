23e715fa490b650696fe676aa31b4921b5384949
Lisandro Di Meo <ddmlisandro22@gmail.com>
2021-02-22 12:16:30 -0300

    Fix previous commit - build failed

--- app/src/main/java/ar/com/wolox/android/example/network/repository/UserRepository.kt
+++ app/src/main/java/ar/com/wolox/android/example/network/repository/UserRepository.kt
@@ -12,15 +12,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class UserRepository @Inject constructor(private val retrofitServices: RetrofitServices, private val userSession: UserSession?) {
+class UserRepository @Inject constructor(private val retrofitServices: RetrofitServices?, private val userSession: UserSession) {
 
     private val service: UserService
-        get() = retrofitServices.getService(UserService::class.java)
+        get() = retrofitServices!!.getService(UserService::class.java)
 
     suspend fun authUser(email: String, password: String) = withContext(Dispatchers.IO) {
         val response = NetworkRequestHandler.safeApiCall { service.authUser(UserAuth(email, password)) }
         if (response is NetworkResponse.Success) {
-            userSession!!.accessToken = response.response.headers()[NetworkHeaders.ACCESS_TOKEN]
+            userSession.accessToken = response.response.headers()[NetworkHeaders.ACCESS_TOKEN]
             userSession.client = response.response.headers()[NetworkHeaders.CLIENT]
             userSession.uid = response.response.headers()[NetworkHeaders.UID]
         }
