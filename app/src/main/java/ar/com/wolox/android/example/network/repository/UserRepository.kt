beec2120b18234fa1ffdef74cf6fa68041a410fa
Lisandro Di Meo <ddmlisandro22@gmail.com>
2021-02-22 12:02:58 -0300

    fix build error

--- app/src/main/java/ar/com/wolox/android/example/network/repository/UserRepository.kt
+++ app/src/main/java/ar/com/wolox/android/example/network/repository/UserRepository.kt
@@ -12,7 +12,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class UserRepository @Inject constructor(private val retrofitServices: RetrofitServices, private val userSession: UserSession) {
+class UserRepository @Inject constructor(private val retrofitServices: RetrofitServices, private val userSession: UserSession?) {
 
     private val service: UserService
         get() = retrofitServices.getService(UserService::class.java)
@@ -20,7 +20,7 @@ class UserRepository @Inject constructor(private val retrofitServices: RetrofitS
     suspend fun authUser(email: String, password: String) = withContext(Dispatchers.IO) {
         val response = NetworkRequestHandler.safeApiCall { service.authUser(UserAuth(email, password)) }
         if (response is NetworkResponse.Success) {
-            userSession.accessToken = response.response.headers()[NetworkHeaders.ACCESS_TOKEN]
+            userSession!!.accessToken = response.response.headers()[NetworkHeaders.ACCESS_TOKEN]
             userSession.client = response.response.headers()[NetworkHeaders.CLIENT]
             userSession.uid = response.response.headers()[NetworkHeaders.UID]
         }
