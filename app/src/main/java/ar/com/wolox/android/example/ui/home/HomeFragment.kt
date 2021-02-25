1cb98182f165c04cfa461fd8664e2b91c0efee16
Lisandro Di Meo <ddmlisandro22@gmail.com>
2021-02-25 14:21:40 -0300

    Tabs icons + color changes on tab change -> improvements !

--- app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
@@ -1,11 +1,11 @@
 package ar.com.wolox.android.example.ui.home
 
+import androidx.core.content.ContextCompat
 import ar.com.wolox.android.R
 import ar.com.wolox.android.databinding.FragmentHomeBinding
 import ar.com.wolox.android.example.ui.home.news.NewsFragment
 import ar.com.wolox.android.example.ui.home.profile.ProfileFragment
 import ar.com.wolox.android.example.ui.login.LoginActivity
-import ar.com.wolox.android.example.utils.enableTabColorChange
 import ar.com.wolox.wolmo.core.adapter.viewpager.SimpleFragmentPagerAdapter
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
 import javax.inject.Inject
@@ -22,31 +22,23 @@ class HomeFragment : WolmoFragment<FragmentHomeBinding, HomePresenter>(), HomeVi
         with(binding) {
             viewPager.adapter = SimpleFragmentPagerAdapter(childFragmentManager).apply {
                 addFragments(
-                        newsFragment to "Page 1",
-                        profileFragment to "Page 2"
+                        newsFragment to "NewsFragment",
+                        profileFragment to "ProfileFragment"
                 )
             }
             tabLayout.setupWithViewPager(viewPager)
-            for (i in 0 until tabLayout.tabCount) {
-                tabLayout.getTabAt(i)!!.apply {
-                    text = getString(TabsData.tabArrayText[i])
-                    setIcon(if (i == 0) TabsData.tabArrayIconsON[i] else TabsData.tabArrayIconsOFF[i])
-                }
+            tabLayout.getTabAt(0)!!.apply {
+                text = getString(R.string.news_fragment_title)
+                setIcon(R.drawable.item_tab_news_selector)
             }
-            tabLayout.enableTabColorChange()
-/*
-* The same code is in "ViewExtensions.kt", left here because I don't know if its better to have it there.
-tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
-                override fun onTabSelected(tab: TabLayout.Tab) {
-                    tab.setIcon(TabsData.tabArrayIconsON[tab.position])
-                }
-
-                override fun onTabUnselected(tab: TabLayout.Tab) {
-                    tab.setIcon(TabsData.tabArrayIconsOFF[tab.position])
-                }
-
-                override fun onTabReselected(tab: TabLayout.Tab) {}
-            })*/
+            tabLayout.getTabAt(1)!!.apply {
+                text = getString(R.string.profile_fragment_title)
+                setIcon(R.drawable.item_tab_profile_selector)
+            }
+            tabLayout.setTabTextColors(
+                    ContextCompat.getColor(requireContext(), R.color.greyTab),
+                    ContextCompat.getColor(requireContext(), R.color.colorAccent)
+            )
         }
     }
 
