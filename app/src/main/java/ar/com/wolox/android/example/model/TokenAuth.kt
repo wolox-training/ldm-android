880fa172ac00cf2959992d96be8a15e4c0938cfc
Lisandro Di Meo <ddmlisandro22@gmail.com>
2021-03-03 16:02:19 -0300

    merge with master

--- app/src/main/java/ar/com/wolox/android/example/model/TokenAuth.kt
+++
@@ -1,9 +0,0 @@
-package ar.com.wolox.android.example.model
-
-import com.google.gson.annotations.SerializedName
-
-data class TokenAuth(
-    @SerializedName("accessToken") val accessToken: String,
-    @SerializedName("uid") val uid: String,
-    @SerializedName("client") val client: String
-)
